ac4c55bc54faa557eee4e4badc1c732a6f8083cd
Awsomenova1 <91033039+Awsomenova1@users.noreply.github.com>
2023-02-04 18:19:50 -0500

    Added multiple moves per turn

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -23,6 +23,7 @@ public class GameController : MonoBehaviour
 
     private int _tilesForVictory = 20;
     private int _turnNumber = 0;
+    private int _movesThisTurn;
 
     private sbyte _numPlayers = 2;
     private sbyte _playerTurn = 0;
@@ -49,6 +50,8 @@ public class GameController : MonoBehaviour
         _random = new Random();
         MapController.GetGameStateFromTilemap(out _tiles, out _zeroIsOddColumn);
         InitializePlayers();
+
+        _movesThisTurn = _players[_playerTurn].NumMoves;
     }
 
     private void OnDestroy()
@@ -77,7 +80,11 @@ public class GameController : MonoBehaviour
         PlayerWon = (sbyte) CheckVictory();
 
         if(_areTilesDirty){
-            NextTurn();
+            _movesThisTurn--;
+
+            if(_movesThisTurn <= 0){
+                NextTurn();
+            }
         }
     }
 
@@ -247,6 +254,8 @@ public class GameController : MonoBehaviour
         if(_playerTurn >= _numPlayers){
             _playerTurn = 0;
         }
+
+        _movesThisTurn = _players[_playerTurn].NumMoves;
     }
 
     private Vector2Int? GetPlayerTreePosition(sbyte playerId)
