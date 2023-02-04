1a4a978ce8f09d7d5b876f065348536be18fc491
Awsomenova1 <91033039+Awsomenova1@users.noreply.github.com>
2023-02-03 23:19:30 -0500

    Added WaterTile, MountainTile, and White OverlayTile sprites

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    private RootTileData[,] _tiles;//TODO keep track of tiles in here
+
+    public struct Player
+    {
+        public int Id;
+        public int TilesControled;
+
+        /// <summary>Number of moves that a player can make in a single turn</summary>
+        public int NumMoves;
+    }
+
+    private int TilesForVictory;
+    private int TurnNumber = 0;
+
+    private int NumPlayers = 2;
+    private int PlayerTurn = 0;
+
+    private Player[] Players;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InitializePlayers();
     }
 
     // Update is called once per frame
@@ -15,4 +34,39 @@ public class GameController : MonoBehaviour
     {
         
     }
+
+    private void InitializePlayers(){
+        Players = new Player[NumPlayers];
+
+        for(int i = 0; i < NumPlayers; i++){
+            Players[i] = new Player
+            {
+                Id = i,
+                TilesControled = 1,
+                NumMoves = 2
+            };
+        }
+    }
+
+    private void NextTurn(){
+        TurnNumber++;
+        PlayerTurn++;
+        if(PlayerTurn >= NumPlayers){
+            PlayerTurn = 0;
+        }
+    }
+
+    private int CheckVictory(){
+        int victory = -1;
+        
+        foreach(Player player in Players){
+            if(player.TilesControled >= TilesForVictory){
+                victory = player.Id;
+            }
+        }
+        // TODO else case for taking base tree and way to check if base tree taken
+
+        return victory;
+    }
+
 }
